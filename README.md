aebd94b4ac1f27c4f89632b45769eec82e4dc11e
Мазманян Юрий <mazmanyan2018@list.ru>
2024-01-21 22:42:59 +0300

    Настройка проекта

---
+++ README.md
@@ -0,0 +1,39 @@
+# Возможные ошибки
+
+    * Обратная совместимость.
+        1. Побробуйте понизить версию, возможно пакет не поддерживает common.js
+        2. Преписать сборку на imoport и export. Пример такой сборки по ссылке <https://github.com/gulpjs/gulp>
+    * Не соответствие структуры проекта
+        1. Проверить структуру папок
+        2. Проверить и преписать структуру
+            - Перейдите в gulpfile.js
+            - Найдите объект paths в нем указана моя структура для обработки файлов
+            - Измениете структуру, которая нужна вам (Дополнительная информаци как это сделать в файле gulpfile.js)
+
+## Инициализация проекта
+
+    * Установка node.js - <https://nodejs.org/en>
+    * Установка gulp глобально - <https://gulpjs.com/docs/en/getting-started/quick-start>
+    * Установка typescript глобально - <https://www.typescriptlang.org/download> или npm install -g typescript
+    * Для установки всех пакетов воспользуйтесь командой - npm init
+    * Перейдите в gulpfile.js
+    * Приступите к настройки нужных типов файлов и нужных зависимостей
+    * Перйдите в пакетный менеджер для добавления нужных расширений - <https://www.npmjs.com/>
+    * Документация сборки - <https://github.com/gulpjs/gulp>
+
+### Пакеты использоавнные в данной сборке
+
+    * gulp-pug - <https://www.npmjs.com/package/gulp-pug>
+    * gulp-htmlmin(Минификация) - <https://www.npmjs.com/package/gulp-htmlmin>
+    * del(Для очистки директорий) - <https://www.npmjs.com/package/del>
+    * gulp-autoprefixer(Для добавление префиксов в стилях) - <https://www.npmjs.com/package/gulp-autoprefixer>
+    * gulp-sass(Препроцессор. Можно изменить) - <https://www.npmjs.com/package/gulp-sass>
+    * gulp-clean-css(Предоставляет файл name и path для дальнейшего анализа.) - <https://www.npmjs.com/package/gulp-clean-css>
+    * gulp-imagemin(Минификация картинок) - <https://www.npmjs.com/package/gulp-imagemin>
+    * gulp-newer(Плагин для Gulp, позволяющий просматривать только те исходные файлы, которые новее соответствующих файлов назначения. ) - <https://www.npmjs.com/package/gulp-newer>
+    * gulp-uglify (Минификация фалов js) - <https://www.npmjs.com/package/gulp-uglify>
+    * gulp-typescript (Работа с typescript) - <https://www.npmjs.com/package/gulp-typescript>
+    * gulp-babel (Транспилирует в старый синтксис js) - <https://www.npmjs.com/package/gulp-babel>
+    * gulp-size (Показывает размер файлов при их обработке) -  <https://www.npmjs.com/package/gulp-size>
+    * browser-sync - <https://browsersync.io/docs/gulp>
+    * jquery(Библиотека) - <https://www.npmjs.com/package/jquery>
---
+++ gulpfile.js
@@ -0,0 +1,167 @@
+const gulp = require("gulp");
+const pugGulp = require("gulp-pug");
+const htmlmin = require("gulp-htmlmin");
+const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+const ts = require("gulp-typescript");
+const size = require("gulp-size");
+const browsersync = require("browser-sync").create();
+const del = require("del");
+const $ = require("jquery");
+
+/**
+ * Данный объект содержит зависимости
+ * Если структура файлов другая просто измениете ее
+ * Если расширения другие или их больше создйте МАССИВ
+ * Пример: paths.styles.src:["ваш путь/*.sass", "ваш путь/*.less"]
+ * Или используйте регулярные выражения
+ */
+
+const paths = {
+  styles: {
+    src: ["src/sass/**/*.sass", "./src/css/**/*.css"],
+    dest: "dist/styles",
+  },
+  scripts: {
+    src: ["src/script/**/*.js", "./src/script/**/*.ts"],
+    dest: "dist/scripts/",
+  },
+  images: {
+    src: "src/images/**/*.{jpg,jpeg,png,svg}",
+    dest: "dist/images",
+  },
+  html: {
+    src: ["src/*.html", "src/*.pug"],
+    dest: "dist/",
+  },
+  pug: {
+    src: "src/*.pug",
+    dest: "dist/",
+  },
+};
+// Очистка дирикторий за исключение изображений
+function clean() {
+  return del(["dist/*", "!dist/images"]);
+}
+
+/**
+ * Обработак стилий(sass).Можнь расширить (less,stylus и тд).
+ * 1.Добавте нужный препроцессор
+ * 2.Добавте нужное расширение в зависимости (paths.styles.src: ["Ваш путь.sass","Ваш путь.less])
+ * 2.Замените .pipe(sass()) на .pipe(less())
+ * Можно изменить путь обратитесь к объекту paths
+ */
+function styles() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(concat("style.min.css"))
+    .pipe(size())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream());
+}
+/**
+ * Обработак script(js и ts).
+ * Можно изменить путь обратитесь к объекту paths
+ */
+function scripts() {
+  return gulp
+    .src(paths.scripts.src, { sourcemaps: true })
+    .pipe(
+      ts({
+        noImplicitAny: true,
+        outFile: "main.min.js",
+      })
+    )
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(uglify())
+    .pipe(concat("main.min.js"))
+    .pipe(size())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browsersync.stream());
+}
+/**
+ * Обработак картинок.
+ * Для расширения возможностей типа фалов обратитесь к объекту
+ * paths.images.src:"src/images/"{jpg,jpeg,png,svg,нужные типы файлов}"
+ * Можно изменить путь обратитесь к объекту paths
+ */
+function images() {
+  return gulp
+    .src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin({ progressiv: true }))
+    .pipe(size())
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browsersync.stream());
+}
+/**
+ * Обработак HTML.
+ * Можно изменить путь обратитесь к объекту paths
+ */
+function html() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size())
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browsersync.stream());
+}
+/**
+ * Обработак расширения pug.
+ * Можно изменить путь обратитесь к объекту paths
+ */
+function pug() {
+  return gulp
+    .src(paths.pug.src)
+    .pipe(pugGulp())
+    .pipe(size())
+    .pipe(gulp.dest(paths.pug.dest))
+    .pipe(browsersync.stream());
+}
+
+function watch() {
+  browsersync.init({
+    server: "./dist/",
+  });
+  gulp.watch(paths.html.dest).on("change", browsersync.reload);
+  gulp.watch(paths.pug.src, pug);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.images.src, images);
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, pug, styles, scripts, images),
+  watch
+);
+
+exports.styles = styles;
+exports.watch = watch;
+exports.clean = clean;
+exports.scripts = scripts;
+exports.html = html;
+exports.images = images;
+exports.pug = pug;
+exports.build = build;
+exports.default = build;
